00681cd0225420c07063ee8254b3f98a707b3595
Long Do <longhoangdo16@gmail.com>
2020-11-23 03:25:53 -0600

    fix

--- data.py
+++ data.py
@@ -174,54 +174,54 @@ class QADataset(Dataset):
 
             # Unpack the context paragraph. Shorten to max sequence length.
             passage = [
-                token for (token, offset) in elem['context_tokens']
-            ]
+                token.lower() for (token, offset) in elem['context_tokens']
+            ][:self.args.max_context_length]
 
             # Each passage has several questions associated with it.
             # Additionally, each question has multiple possible answer spans.
             for qa in elem['qas']:
 
-                non_tokenized_context = ' '.join(passage)
-                non_tokenized_question = qa['question']
+                # non_tokenized_context = ' '.join(passage)
+                # non_tokenized_question = qa['question']
 
-                roots_question = []
-                roots_context = []
+                # roots_question = []
+                # roots_context = []
 
-                doc_context = nlp(non_tokenized_context)
-                doc_question = nlp(non_tokenized_question)
+                # doc_context = nlp(non_tokenized_context)
+                # doc_question = nlp(non_tokenized_question)
 
-                question_name_ents = [str(ent) for ent in doc_question.ents]
+                # question_name_ents = [str(ent) for ent in doc_question.ents]
 
-                temp = ''
+                # temp = ''
 
-                for sent in doc_question.sents:
-                    roots_question =[st.stem(chunk.root.head.text.lower()) for chunk in sent.noun_chunks]
+                # for sent in doc_question.sents:
+                #     roots_question =[st.stem(chunk.root.head.text.lower()) for chunk in sent.noun_chunks]
 
-                for sent in doc_context.sents:
-                    roots_context = [st.stem(chunk.root.head.text.lower()) for chunk in sent.noun_chunks]
+                # for sent in doc_context.sents:
+                #     roots_context = [st.stem(chunk.root.head.text.lower()) for chunk in sent.noun_chunks]
 
-                    context_name_ents = [str(ent) for ent in sent.ents]
+                #     context_name_ents = [str(ent) for ent in sent.ents]
 
-                    added = False
+                #     added = False
 
-                    for root in roots_question:
-                        if root in roots_context:
-                            temp += str(sent) + ' '
-                            added = True
-                            break
+                #     for root in roots_question:
+                #         if root in roots_context:
+                #             temp += str(sent) + ' '
+                #             added = True
+                #             break
 
-                    if not added:
-                        for q_ent in question_name_ents:
-                            for c_ent in context_name_ents:
-                                if q_ent in c_ent or c_ent in q_ent:
-                                    temp += str(sent) + ' '
-                                    break
+                #     if not added:
+                #         for q_ent in question_name_ents:
+                #             for c_ent in context_name_ents:
+                #                 if q_ent in c_ent or c_ent in q_ent:
+                #                     temp += str(sent) + ' '
+                #                     break
 
-                temp = temp[0: - 1]
+                # temp = temp[0: - 1]
 
-                temp = temp.split(' ')
+                # temp = temp.split(' ')
 
-                passage = [chunk.lower() for chunk in temp][:self.args.max_context_length]
+                # passage = [chunk.lower() for chunk in temp][:self.args.max_context_length]
 
                 qid = qa['qid']
                 question = [
