681ab4c3fd3f1048e0c04c512491670b4e0a4861
Long Do <longhoangdo16@gmail.com>
2020-11-23 13:12:46 -0600

    fix

--- data.py
+++ data.py
@@ -227,7 +227,6 @@ class QADataset(Dataset):
                     if not added:
                         if len(context_name_ents) > 0:
                             temp += str(sent) + ' '
-                            break
 
                 temp = temp[0: - 1]
 
